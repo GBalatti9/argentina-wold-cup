22e61899d0aaa013b50a5756b8cc0c143732aa8f
GBalatti9 <gas.balatti@gmail.com>
2024-01-08 19:40:42 -0300

    Staff title

--- src/staff/pages/StaffPage.jsx
+++ src/staff/pages/StaffPage.jsx
@@ -4,6 +4,8 @@ import { PlayersAndStaffList } from "../components/PlayersAndStaffList"
 export const StaffPage = () => {
     return (
         <>
+            <h1>Staff</h1>
+            <hr />
             <PlayersAndStaffList role={ 'staff' } />
         </>
     )
