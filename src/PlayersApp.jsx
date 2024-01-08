54b6c6655bc97742d4e0b2e9497de7adc3f60d09
GBalatti9 <gas.balatti@gmail.com>
2024-01-08 17:23:11 -0300

    Setting up the project

---
+++ src/PlayersApp.jsx
@@ -0,0 +1,7 @@
+
+
+export const PlayersApp = () => {
+    return (
+        <div>PlayersApp</div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { PlayersApp } from './PlayersApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PlayersApp />
+  </React.StrictMode>,
+)
